8fde16deedfb2df5b2ef39983a6a7709a0395f75
Daavox <47478631+Daavox@users.noreply.github.com>
2020-05-08 13:45:53 +0200

    Add files via upload

---
+++ ModKom/Anmeldefenster.java
@@ -0,0 +1,108 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.font.TextAttribute;
+import java.util.Hashtable;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 09.03.2020
+ * @author 
+ */
+
+public class Anmeldefenster extends JFrame {
+  // Anfang Attribute
+  
+  private JLabel lModKom = new JLabel();
+  private JLabel lHerzlichWillkommen = new JLabel();
+  private JTextField jTextField1 = new JTextField();
+  private JTextField jTextField2 = new JTextField();
+  private JLabel lBenutzername = new JLabel();
+  private JLabel lPasswort = new JLabel();
+  private JButton bLogin = new JButton();
+  private JButton bNochKeinenAccount = new JButton();
+  // Ende Attribute
+  
+  public Anmeldefenster() { 
+    // Frame-Initialisierung
+    super();
+    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    int frameWidth = 1280; 
+    int frameHeight = 720;
+    setSize(frameWidth, frameHeight);
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (d.width - getSize().width) / 2;
+    int y = (d.height - getSize().height) / 2;
+    setLocation(x, y);
+    setTitle("Anmeldefenster");
+    setResizable(false);
+    Container cp = getContentPane();
+    cp.setLayout(null);
+    // Anfang Komponenten
+    
+    
+    lModKom.setBounds(50, 18, 110, 33);
+    lModKom.setText("ModKom");
+    lModKom.setFont(new Font("Dialog", Font.BOLD, 24));
+    cp.add(lModKom);
+    lHerzlichWillkommen.setBounds(50, 60, 207, 28);
+    lHerzlichWillkommen.setText("Herzlich Willkommen!");
+    lHerzlichWillkommen.setFont(new Font("Dialog", Font.BOLD, 20));
+    cp.add(lHerzlichWillkommen);
+    jTextField1.setBounds(233, 122, 150, 20);
+    cp.add(jTextField1);
+    jTextField2.setBounds(233, 167, 150, 20);
+    cp.add(jTextField2);
+    lBenutzername.setBounds(70, 122, 110, 20);
+    lBenutzername.setText("Benutzername");
+    cp.add(lBenutzername);
+    lPasswort.setBounds(70, 167, 110, 20);
+    lPasswort.setText("Passwort");
+    cp.add(lPasswort);
+    bLogin.setBounds(306, 210, 75, 25);
+    bLogin.setText("Login");
+    bLogin.setMargin(new Insets(2, 2, 2, 2));
+    bLogin.addActionListener(new ActionListener() { 
+      public void actionPerformed(ActionEvent evt) { 
+        bLogin_ActionPerformed(evt);
+        Chats test = new Chats();
+      }
+    });
+    cp.add(bLogin);
+    bNochKeinenAccount.setBounds(68, 210, 147, 25);
+    bNochKeinenAccount.setText("Noch Keinen Account?");
+    bNochKeinenAccount.setMargin(new Insets(2, 2, 2, 2));
+    bNochKeinenAccount.addActionListener(new ActionListener() { 
+      public void actionPerformed(ActionEvent evt) { 
+        bNochKeinenAccount_ActionPerformed(evt);
+        accountErstellen test2 = new accountErstellen();
+      }
+    });
+    cp.add(bNochKeinenAccount);
+    // Ende Komponenten
+    
+    setVisible(true);
+  } // end of public Anmeldefenster
+  
+  // Anfang Methoden
+  
+  public static void main(String[] args) {
+    new Anmeldefenster();
+  } // end of main
+  
+  public void bLogin_ActionPerformed(ActionEvent evt) {
+    // TODO hier Quelltext einfügen
+    
+  } // end of bLogin_ActionPerformed
+
+  public void bNochKeinenAccount_ActionPerformed(ActionEvent evt) {
+    // TODO hier Quelltext einfügen
+    
+  } // end of bNochKeinenAccount_ActionPerformed
+
+  // Ende Methoden
+} // end of class Anmeldefenster
+
---
+++ ModKom/accountErstellen.java
@@ -0,0 +1,89 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.font.TextAttribute;
+import java.util.Hashtable;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 09.03.2020
+ * @author 
+ */
+
+public class accountErstellen extends JFrame {
+  // Anfang Attribute
+  
+  private JLabel lErstellenSiesicheinenAccountindemSieeinenBenutzernamenundeinPassworteingeben = new JLabel();
+  private JLabel lBenutzername = new JLabel();
+  private JLabel lPasswort = new JLabel();
+  private JTextField jTextField1 = new JTextField();
+  private JTextField jTextField2 = new JTextField();
+  private JButton bAccounterstellen = new JButton();
+  // Ende Attribute
+  
+  public accountErstellen() { 
+    // Frame-Initialisierung
+    super();
+    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    int frameWidth = 1280; 
+    int frameHeight = 720;
+    setSize(frameWidth, frameHeight);
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (d.width - getSize().width) / 2;
+    int y = (d.height - getSize().height) / 2;
+    setLocation(x, y);
+    setTitle("Account erstellen");
+    setResizable(false);
+    Container cp = getContentPane();
+    cp.setLayout(null);
+    // Anfang Komponenten
+    
+    
+    lErstellenSiesicheinenAccountindemSieeinenBenutzernamenundeinPassworteingeben.setBounds(41, 23, 821, 25);
+    lErstellenSiesicheinenAccountindemSieeinenBenutzernamenundeinPassworteingeben.setText("Erstellen Sie sich einen Account indem Sie einen Benutzernamen und ein Passwort eingeben");
+    lErstellenSiesicheinenAccountindemSieeinenBenutzernamenundeinPassworteingeben.setFont(new Font("Dialog", Font.BOLD, 18));
+    cp.add(lErstellenSiesicheinenAccountindemSieeinenBenutzernamenundeinPassworteingeben);
+    lBenutzername.setBounds(95, 88, 110, 20);
+    lBenutzername.setText("Benutzername");
+    cp.add(lBenutzername);
+    lPasswort.setBounds(98, 150, 110, 20);
+    lPasswort.setText("Passwort");
+    cp.add(lPasswort);
+    jTextField1.setBounds(260, 92, 150, 20);
+    cp.add(jTextField1);
+    jTextField2.setBounds(263, 153, 150, 20);
+    cp.add(jTextField2);
+    bAccounterstellen.setBounds(300, 205, 115, 25);
+    bAccounterstellen.setText("Account erstellen");
+    bAccounterstellen.setMargin(new Insets(2, 2, 2, 2));
+    bAccounterstellen.addActionListener(new ActionListener() { 
+      public void actionPerformed(ActionEvent evt) { 
+        bAccounterstellen_ActionPerformed(evt);
+        //Nachdem der User einen Benutzername und ein Passwort eingegeben hat sollen diese Strings auf eine
+        //Variable geschrieben werden und an die Klasse Anmeldefenster geschickt werden, damit überprüft werden
+        //kann ob diese richtig sind und dann soll der User Zugang bekommen
+      }
+    });
+    cp.add(bAccounterstellen);
+    // Ende Komponenten
+    
+    setVisible(true);
+  } // end of public BankOverlay
+  
+  // Anfang Methoden
+  
+  public static void main(String[] args) {
+    new accountErstellen();
+  } // end of main
+  
+  public void bAccounterstellen_ActionPerformed(ActionEvent evt) {
+    // TODO hier Quelltext einfügen
+    
+  } // end of bAccounterstellen_ActionPerformed
+
+  // Ende Methoden
+} // end of class BankOverlay
+
